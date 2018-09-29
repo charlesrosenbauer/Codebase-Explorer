113714568d6497ffe70f46d860c27da699742808
charlesrosenbauer <charles.rosenbauer@gmail.com>
2018-09-28 23:52:18 -0500

    Added in some stuff for file correlations.

--- src/reader.go
+++ src/reader.go
@@ -5,6 +5,7 @@ import(
   "regexp"
   "math"
   "strings"
+  "math/rand"
 )
 
 
@@ -13,6 +14,7 @@ type ProgramMap struct{
   Filepath string
   Wordcts  map[string]int
   Wordwts  map[string]float32
+  Totalwt  float32
   Totalct  int
 }
 
@@ -55,6 +57,7 @@ func (p *ProgramMap)GetLocalWeights(){
 
     val = val * factor
     p.Wordwts[i] = val
+    p.Totalwt += val
   }
 }
 
@@ -74,6 +77,81 @@ func MakeGlobalMap(maps []ProgramMap) ProgramMap {
       }
       ret.Totalct += m.Wordcts[v]
     }
+  }
+  return ret
+}
+
+
+func ComputeCorrelation(a, b *ProgramMap) float32 {
+
+  var x map[string]float32
+  var y map[string]float32
+  var totalA float32
+  var totalB float32
+  if len(a.Wordwts) < len(b.Wordwts) {
+    x = a.Wordwts
+    y = b.Wordwts
+    totalA = a.Totalwt
+    totalB = b.Totalwt
+  }else{
+    x = b.Wordwts
+    y = a.Wordwts
+    totalA = b.Totalwt
+    totalB = a.Totalwt
+  }
+
+  var accuma float32 = 0.0
+  var accumb float32 = 0.0
+  for i, v := range x {
+    if wgt, ok := y[i]; ok {
+      accuma += wgt
+      accumb += v
+    }
+  }
+
+  parta := accuma / totalA
+  partb := accumb / totalB
+
+  return float32(math.Sqrt(float64(parta * partb)))
+}
+
+
+
+
+
+type FileData struct{
+  Path string
+  X, Y float32
+}
+
+type ClusterData struct{
+  Files []FileData
+  Edges [][]float32
+  Size  int
+}
+
+
+
+func MakeClusterData(maps []ProgramMap) ClusterData {
+
+  var ret ClusterData
+  ret.Size = len(maps)
+
+  ret.Files = make([]FileData, ret.Size)
+  ret.Edges = make([][]float32, ret.Size)
+  for i := 0; i < ret.Size; i++ {
+    ret.Edges[i] = make([]float32, ret.Size);
+  }
+
+  for i, va := range maps {
+    for j, vb := range maps {
+      if i < j {
+        ret.Edges[i][j] = ComputeCorrelation(&va, &vb)
+      }else{
+        ret.Edges[i][j] = 0.0
+      }
+    }
+    ret.Files[i] = FileData{va.Filepath, rand.Float32(), rand.Float32()}
   }
   return ret
 }
--- src/render.go
+++ src/render.go
@@ -11,7 +11,8 @@ import (
 
 
 
-func (s *Surface)DrawNode(x, y int, highlight uint8){
-  r := Rect{x, y, 5, 5}
-  s.FillRect(r, uint32(255 - highlight))
+
+func DrawNode(x, y int32, highlight uint8, s *sdl.Surface){
+  r := sdl.Rect{x, y, 5, 5}
+  s.FillRect(&r, uint32(255 - highlight))
 }
